e1fa3bade0ea8f223c3713ceced24ed516b9e8a0
DevilDragon05 <93267927+DevilDragon05@users.noreply.github.com>
2023-12-23 19:22:30 +0530

    Done with the Project

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,31 @@
-/**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+"use strict";
+
+const api_key = "b00f6392d355726b2bd029c7046482ed";
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+// {string} URL API url
+// {Function} callback 
+
+export const url ={
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon){
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon){
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    // {string} query Search query example: "London", "New York"
+    geo(query){
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
